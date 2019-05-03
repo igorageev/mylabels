48065d35396d75e7ddf3d7ca7592fc4711f0c6f6
igor.ageev <igor@ageev.online>
2019-05-03 12:55:48 +0300

    Part of code moved outside of main Vue instance

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,48 +1,62 @@
 <template>
-  <nav class="uk-navbar-container uk-margin uk-background-primary uk-light uk-navbar-transparent" uk-navbar>
-    <div class="uk-navbar-left">
-      <a class="uk-navbar-item uk-logo" href="#">{{ name }}</a>
-    </div>
-    <div class="uk-navbar-right">
-      <div class="uk-navbar-item">
-        <form action="javascript:void(0)">
-          <div uk-form-custom>
-            <input type="file" @change="changeData($event)" accept=".csv">
-            <button class="uk-button uk-button-default" type="button" tabindex="-1">Open</button>
-          </div>
-          <button class="uk-button uk-button-default" type="button" uk-toggle="target: #modal">Setup</button>
-          <button v-on:click="window.print()" class="uk-button uk-button-default" type="button">Print</button>
-        </form>
+  <div class="uk-active uk-position-top uk-position-fixed">
+    <nav
+      class="uk-navbar-container uk-margin uk-background-primary uk-light uk-navbar-transparent"
+      uk-navbar
+    >
+      <div class="uk-navbar-left">
+        <a class="uk-navbar-item uk-logo" href="#">{{ name }}</a>
       </div>
-    </div>
-  </nav>
+      <div class="uk-navbar-right">
+        <div class="uk-navbar-item">
+          <form action="javascript:void(0)">
+            <div uk-form-custom>
+              <input type="file" @change="parse($event)" accept=".csv">
+              <button class="uk-button uk-button-default" type="button" tabindex="-1">Open</button>
+            </div>
+            <button
+              class="uk-button uk-button-default"
+              type="button"
+              uk-toggle="target: #modal">
+                Setup
+            </button>
+            <button @click.prevent="print()" class="uk-button uk-button-default" type="button">Print</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: '',
-    props: {
-      name: String
+import Papa from 'papaparse'
+
+export default {
+  name: 'NavBar',
+  props: {
+    name: String
+  },
+  methods: {
+    // Get data from CSV file
+    parse: function (event) {
+      // Check for the various File API support.
+      if (window.FileReader && event.target.files) {
+        var self = this
+        // FileReader are supported.
+        Papa.parse(event.target.files[0], {
+          header: true,
+          complete: function (results) {
+            self.$emit('dataReceived', results.data)
+          }
+        })
+      } else {
+        alert('FileReader are not supported in this browser.')
+      }
+    },
+    // no comments
+    print: function () {
+      window.print()
     }
   }
+}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="less">
-  @font-face {
-    font-family: 'Lobster';
-    src: url('../assets/fonts/lobster-regular-webfont.woff2') format('woff2'), url('../assets/fonts/lobster-regular-webfont.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
-  }
-  
-  .uk-logo {
-    font-family: 'Lobster';
-    font-size: 2rem;
-    color: rgba(255, 255, 255, .7);
-  }
-  
-  nav button {
-    margin-right: .25rem;
-  }
-</style>
---
+++ src/components/SetupModal.vue
@@ -0,0 +1,115 @@
+<template>
+  <div id="modal" uk-modal>
+    <div class="uk-modal-dialog uk-modal-body">
+      <h2 class="uk-modal-title">Setup</h2>
+      <form class="uk-grid-small" uk-grid>
+        <div class="uk-width-1-2@s">
+          <label class="uk-form-label" for="numbersize">
+            ID font size
+            <span class="uk-text-muted">(pt)</span>
+          </label>
+          <input
+            v-model="layout.numberSize"
+            @input="setupLayout"
+            id="numbersize"
+            class="uk-input"
+            type="text"
+            placeholder="22"
+          >
+        </div>
+        <div class="uk-width-1-2@s">
+          <label class="uk-form-label" for="fontsize">
+            Descriptions font size
+            <span class="uk-text-muted">(pt)</span>
+          </label>
+          <input
+            v-model="layout.textSize"
+            @input="setupLayout"
+            class="uk-input"
+            type="text"
+            placeholder="8"
+          >
+        </div>
+
+        <div class="uk-width-1-2@s">
+          <label class="uk-form-label" for="numbersize">
+            Top margin of the page
+            <span class="uk-text-muted">(mm)</span>
+          </label>
+          <input
+            v-model="layout.topMargin"
+            @input="setupLayout"
+            id="numbersize"
+            class="uk-input"
+            type="text"
+            placeholder="22"
+          >
+        </div>
+        <div class="uk-width-1-2@s">
+          <label class="uk-form-label" for="fontsize">
+            Left margin of the page
+            <span class="uk-text-muted">(mm)</span>
+          </label>
+          <input
+            v-model="layout.leftMargin"
+            @input="setupLayout"
+            class="uk-input"
+            type="text"
+            placeholder="8"
+          >
+        </div>
+
+        <div class="uk-width-1-2@s">
+          <label>
+            <input v-model="isBorder" class="uk-checkbox" @change="setupLayout" type="checkbox" checked> Show border
+          </label>
+        </div>
+      </form>
+      <p class="uk-text-right">
+        <button class="uk-button uk-button-primary uk-modal-close" type="button">Close</button>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SetupModal',
+  data: function () {
+    return {
+      layout: {
+        numberSize: 22,
+        textSize: 8,
+        topMargin: 22,
+        leftMargin: 8
+      },
+      theme: 'border',
+      isBorder: true,
+      style: document.createElement('style')
+    }
+  },
+  created: function () {
+    document.body.append(this.style)
+    this.setupLayout()
+  },
+  methods: {
+    setupLayout: function () {
+      var lineHeightHead = this.layout.numberSize > 15
+        ? Math.floor(this.layout.numberSize * 0.85)
+        : Math.floor(this.layout.numberSize * 1.2)
+      var lineHeightText = this.layout.numberSize > 15
+        ? Math.floor(this.layout.textSize * 0.85)
+        : Math.floor(this.layout.textSize * 1.2)
+      var border = this.isBorder
+        ? '.border {border-top-color: lightgray; border-left-color: lightgray;}' +
+          '.border .label {border-bottom-color: lightgrey;border-right-color: lightgrey;}'
+        : ''
+      this.style.innerText =
+        border +
+        '.label h1 {font-size: ' + this.layout.numberSize + 'pt; line-height: ' + lineHeightHead + 'pt;}' +
+        '.label p {font-size: ' + this.layout.textSize + 'pt; line-height: ' + lineHeightText + 'pt;}' +
+        '@media print { .container {margin: ' + this.layout.topMargin + 'mm ' + this.layout.leftMargin + 'mm;} }'
+    }
+  }
+}
+</script>
